52bd6beddfddb24c95c3b0e7143fc9789a24f9b9
sudeshyalavarthi <sudeshyalavarthi@gmail.com>
2022-11-10 13:32:15 +0000

    static_files test

--- init.py
+++ init.py
@@ -7,7 +7,7 @@ import os
 import converter
 
 app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, root="")
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="/static")
 
 def test():
     print("Hello")
